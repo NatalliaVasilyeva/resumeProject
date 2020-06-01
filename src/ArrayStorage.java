77a352065b1ba4db61ba490bd5fe45a540a90bf3
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-06-01 16:33:49 +0300

    HW1 feat: implement array methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,58 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = null;
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (Objects.equals(resume.uuid, uuid)) return resume;
+            break;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (Objects.equals(storage[i].uuid, uuid)) {
+                Resume[] copy = new Resume[storage.length];
+                System.arraycopy(storage, 0, copy, 0, i);
+                System.arraycopy(storage, i + 1, copy, i, storage.length - i - 1);
+                copy[copy.length - 1] = null;
+                storage = copy;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count=0;
+        while (storage[count]!=null) {
+            count++;
+        }
+        Resume[] copy = new Resume[count];
+        System.arraycopy(storage, 0, copy, 0, count);
+        return copy;
     }
 
     int size() {
-        return 0;
+        int count=0;
+        while (storage[count]!=null) {
+            count++;
+        }
+        return count;
     }
 }
